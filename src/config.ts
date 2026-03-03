e5212eb1cb21ea16a0fde6eda7f9c3463c515c77
Turan <turan@Turans-MacBook-Pro.local>
2026-03-03 23:33:51 +0400

    latest

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const envSchema = z.object({
-    EMAIL_USER: z.string().email(),
+    EMAIL_USER: z.email(),
     EMAIL_PASS:z.string().min(1),
-    SUPABASE_URL:z.string().url(),
+    SUPABASE_URL:z.url(),
     SUPABASE_ANON_KEY:z.string().min(1),
     API_KEY:z.string().min(1)
 });
